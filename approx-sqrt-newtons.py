c1b366a5ae772fddaf58b6c39085829ec54aff02
Claire Drummond <g00364781@gmit.ie>
2019-09-29 20:45:28 +0100

    First draft of program

---
+++ approx-sqrt-newtons.py
@@ -0,0 +1,13 @@
+ # adapted from: https://tour.golang.org/flowcontrol/8
+
+def sqrt(x):
+    # initial guess.
+    z = 1.0
+    # Keep getting a better estimate for the square root of x until you are withintwo decimal places
+    while abs(z*z - x) >= 0.01
+        # get a better approximation for the sqaure root
+        z -= (z*z - x) / (2*z)
+
+    return z
+
+sqrt(8.0)
