13a9fb5ab3f486ad0bac2eac51aa559fddf7dd04
Claire Drummond <g00364781@gmit.ie>
2019-09-29 20:49:22 +0100

    added colon

--- approx-sqrt-newtons.py
+++ approx-sqrt-newtons.py
@@ -4,7 +4,7 @@ def sqrt(x):
     # initial guess.
     z = 1.0
     # Keep getting a better estimate for the square root of x until you are withintwo decimal places
-    while abs(z*z - x) >= 0.01
+    while abs(z*z - x) >= 0.01:
         # get a better approximation for the sqaure root
         z -= (z*z - x) / (2*z)
 
